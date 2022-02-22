93281bf5997de5fcc8d1298a0a70dbf559c8dd43
daltonfranco <daltonpacolafranco.git@hotmail.com>
2022-02-22 17:49:46 +0000

    alterações

---
+++ Empreado.java
@@ -0,0 +1,14 @@
+public class Empreado extends Pessoa {
+    
+    private double salario;
+
+    
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+}
