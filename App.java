c935c2ba081af5862527086250861de856ac17e9
daltonfranco <daltonpacolafranco.git@hotmail.com>
2022-02-22 18:51:34 +0000

    alteracoes

--- App.java
+++ App.java
@@ -1,3 +1,28 @@
-public class App {
-    
+public class App{
+
+    public static void main(String[] args){
+
+        Empregado empregado = new Empregado();
+
+        empregado.setNome("Josué, o Brabo");
+        empregado.setEndereco("Avenida Paula Noku Santos");
+        empregado.setTelefone("121212121212");
+        empregado.setSalario(4000.00);
+
+
+        Empregado empregado2 = new Empregado();
+
+        empregado2.setNome("Dalton");
+        empregado2.setEndereco("Tupa");
+        empregado2.setTelefone("14998268042");
+        empregado2.setSalario(0);
+
+
+        System.out.println(empregado2.getNome());
+        System.out.println(empregado2.getEndereco());
+        System.out.println(empregado2.getTelefone());
+        System.out.println(empregado2.getSalario() + " Reais");
+
+    }
+
 }
--- Empregado.java
+++ Empregado.java
@@ -1,14 +1,16 @@
-public class Empreado extends Pessoa {
+public class Empregado extends Pessoa {
     
     private double salario;
 
-    
+
     public double getSalario() {
+        
         return salario;
     }
 
     public void setSalario(double salario) {
         this.salario = salario;
     }
+
 
 }
